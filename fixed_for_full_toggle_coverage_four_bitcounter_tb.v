972c2651a88d81b9629607ce20a76b453c8330cf
Shekhar_Shwetank <shwetankshekharcode@gmail.com>
2025-07-21 10:43:39 +0530

    Update fixed_for_full_toggle_coverage_four_bitcounter_tb.v

--- fixed_for_full_toggle_coverage_four_bitcounter_tb.v
+++ fixed_for_full_toggle_coverage_four_bitcounter_tb.v
@@ -1,10 +1,11 @@
 module four_bitcounter_tb();
 	reg clk,rst;
-	wire reg[3:0]q;
+	wire [3:0]q;
 
-##initiating the top module
+//initiating the top module
 four_bitcounter UUT(.clk(clk),.rst(rst),.q(q));
 
+initial clk=0;
 always #5 clk=~clk;
 
 initial
@@ -16,10 +17,11 @@ initial
 		
 		//Apply reset again mid-count
 		rst = 1'b1;
-		#10;
-		rst = 1'b0;
+		#10; //For 100% Code Coverage(Toggel Coverage), so that the clk cycle is registered, can be any amout of delay,like #2 or #3...
+		/*
+  		rst = 1'b0;
 		repeat(5)@(posedge clk); //Final increment cycles
-		
+		*/
 		//End of simulation
 		$finish; 
 	end
