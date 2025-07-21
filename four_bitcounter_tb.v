ef967cd053b81c524ca8741c7ef4d1883a135002
Shwetank <shwetankshekharcode@gmail.com>
2025-07-21 10:49:12 +0530

    fixed counter syntax issue

--- four_bitcounter_tb.v
+++ four_bitcounter_tb.v
@@ -2,7 +2,7 @@ module four_bitcounter_tb();
 	reg clk,rst;
 	wire reg[3:0]q;
 
-##initiating the top module
+//initiating the top module
 four_bitcounter UUT(.clk(clk),.rst(rst),.q(q));
 
 always #5 clk=~clk;
